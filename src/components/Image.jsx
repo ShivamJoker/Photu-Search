e297a2ca7470c37543cf9dd920c50d6732af9378
Shivam <singhshivamkr@gmail.com>
2020-12-20 14:23:05 +0530

    fix

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Image = ({ urls, handleClick }) => {
-  console.log(rootDOMAttributes);
+
   return (
     <li className="img" onClick={() => handleClick(urls.regular)}>
       <img src={urls.small} alt="Picsum photo" width="250" />
